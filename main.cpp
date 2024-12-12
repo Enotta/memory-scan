0567255885b93474eec2de653bbf5ab9955672f3
Enotta <nvageev_1@edu.hse.ru>
2024-12-12 21:35:56 +0300

    directory tds started

--- main.cpp
+++ main.cpp
@@ -5,20 +5,43 @@
 
 using namespace std;
 
-bool validate_path(string path) {
+void trim_path(string* path) {
+    if ((*path).rfind('"', 0) == 0)
+        *path = path->substr(1, path->length()-2);
+}
+
+void tds_folder_contents(string path, uint16_t level) {
+    trim_path(&path);
+
+    string level_string = "";
+    for (const auto & entry : filesystem::directory_iterator(path)) {
+        filesystem::path local_path = entry.path();
+        level_string.clear();
+        for (int i = 0; i < level; ++i) level_string += "--";
+        std::cout << level_string << " " << local_path << std::endl;
+
+        if (filesystem::is_directory(local_path)) tds_folder_contents(local_path.string(), level+1);
+    }
+}
+
+bool validate_path(string* path) {
+    trim_path(path);
+
     struct stat sb;
-    return stat(path.substr(1, path.length()-2).c_str(), &sb) == 0;
+    return stat(path->c_str(), &sb) == 0;
 }
 
 int main() {
     string path = "";
 
-    while (path == "" || !validate_path(path)) {
+    // Проверка пути на существование
+    while (path == "" || !validate_path(&path)) {
         cout << "Введите путь до директории (самый простой способ - скопировать в проводнике): ";
         cin >> path;
     }
 
-    cout << "Путь валиден!";
+    // Поиск в глубину всего, что есть в папке
+    tds_folder_contents(path, 0);
 
     return 0;
 }
